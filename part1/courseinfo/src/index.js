7cdbc2d0088f4a90321df10395fa1e13ffa6d709
ElMayo <mic.l.meier@gmail.com>
2020-03-16 10:42:08 +0100

    exercises - courseinfo step 9

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -31,7 +31,9 @@ const Total = ({ course }) => {
 
   return (
     <div>
-      <p>Number of exercises: {total}</p>
+      <p>
+        <b>Number of exercises: {total}</b>
+      </p>
     </div>
   );
 };
@@ -47,31 +49,56 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course, i) => (
+        <Course key={i} course={course} />
+      ))}
     </div>
   );
 };
