9e0a58e194baf4f48ffa03c7810721aabe77315c
ElMayo <mic.l.meier@gmail.com>
2020-03-17 15:55:54 +0100

    exercise phonebook - step6

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ContactForm from "./ContactForm";
 import Persons from "./Persons";
 import SearchField from "./SearchField";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchName, setSearchName] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(res => {
+      setPersons(res.data);
+    });
+  }, []);
 
   const handleSearchNameChange = event => {
     setSearchName(event.target.value);
