dcc310e72be2f6ba93a4c44a3108d82e06ffd290
ElMayo <mic.l.meier@gmail.com>
2020-03-17 11:39:48 +0100

    exercise phonebook - step4

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "777-777-77-77" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
+
+  const handleSearchNameChange = event => {
+    setSearchName(event.target.value);
+  };
 
   const handleContactChange = event => {
     setNewName(event.target.value);
@@ -35,6 +43,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input value={searchName} onChange={handleSearchNameChange} />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addContact}>
         <div>
           Name: <input value={newName} onChange={handleContactChange} />
@@ -48,11 +60,15 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => (
-          <div key={person.name}>
-            {person.name} {person.number}
-          </div>
-        ))}
+        {persons
+          .filter(person =>
+            person.name.toUpperCase().includes(searchName.toUpperCase())
+          )
+          .map(person => (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          ))}
       </div>
     </div>
   );
