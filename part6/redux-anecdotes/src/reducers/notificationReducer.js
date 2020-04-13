39513bc9190301ad559146a030ba287b7d70e8b1
ElMayo <mic.l.meier@gmail.com>
2020-04-13 11:20:09 +0200

    exercise better anecdotes - step8 solved

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,5 @@
+const reducer = (state = "TODO", action) => {
+  return state;
+};
+
+export default reducer;
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,8 +2,12 @@ import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import anecdoteReducer from "./reducers/anecdoteReducer";
+import notificationReducer from "./reducers/notificationReducer";
 
-const reducer = combineReducers({ anecdotes: anecdoteReducer });
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer,
+});
 
 const store = createStore(reducer, composeWithDevTools());
 
